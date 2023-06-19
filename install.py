9f2fc82ba8eee17d029515702e20b29c1ef3e0c0
DenkingOfficial <shershnev.andrei@gmail.com>
2023-06-19 21:11:26 +0500

    Initial commit

---
+++ install.py
@@ -0,0 +1,27 @@
+# taken from here https://github.com/Mikubill/sd-webui-controlnet/blob/main/install.py
+import os
+import launch
+import pkg_resources
+
+
+req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+with open(req_file) as file:
+    for package in file:
+        try:
+            package = package.strip()
+            if "==" in package:
+                package_name, package_version = package.split("==")
+                installed_version = pkg_resources.get_distribution(package_name).version
+                if installed_version != package_version:
+                    launch.run_pip(
+                        f"install {package}",
+                        f"sd-telegram-bot-extension requirement: changing {package_name} version from {installed_version} to {package_version}",
+                    )
+            elif not launch.is_installed(package):
+                launch.run_pip(
+                    f"install {package}",
+                    f"sd-telegram-bot-extension requirement: {package}",
+                )
+        except Exception as e:
+            print(e)
+            print(f"Warning: Failed to install {package}")
